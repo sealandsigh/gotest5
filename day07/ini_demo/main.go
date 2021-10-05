21a13c32a7d59c6bca957fee44afc44602f5888f
sealandsigh <zhujiajunzhu@qq.com>
2021-10-05 16:15:10 +0800

    day07 ini配置文件解释完成

--- day07/ini_demo/main.go
+++ day07/ini_demo/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -60,6 +61,10 @@ func loadIni(filename string, data interface{}) (err error) {
 	for idx, line := range lineSlice {
 		// 去掉字符串首尾空格
 		line = strings.TrimSpace(line)
+		// 如果是空行就跳过
+		if len(line) == 0 {
+			continue
+		}
 		// 2.1 如果是注释就跳过
 		if strings.HasPrefix(line, ";") || strings.HasPrefix(line, "#") {
 			continue
@@ -88,6 +93,72 @@ func loadIni(filename string, data interface{}) (err error) {
 
 		} else {
 			// 2.3 如果不是[开头就是=分割的键值对]
+			// 1 以等号分割这一行，等号左边是key,等号右边是value
+			if !strings.Contains(line, "=") || strings.HasPrefix(line, "=") {
+				err = fmt.Errorf("line:%d syntax error", idx+1)
+				return
+			}
+			index := strings.Index(line, "=")
+			key := strings.TrimSpace(line[:index])
+			value := strings.TrimSpace(line[index+1:])
+			// 2 根据strucName去data里面把对应的嵌套结构体取出来
+			v := reflect.ValueOf(data)
+			sValue := v.Elem().FieldByName(structName) // 拿到嵌套结构体信息
+			sType := sValue.Type()                     // 拿到嵌套结构体的类型信息
+
+			if sType.Kind() != reflect.Struct {
+				err = fmt.Errorf("data中的%s字段应该是一个结构体", structName)
+				return
+			}
+			var fieldName string
+			var fileType reflect.StructField
+			// 3 遍历嵌套结构体的每一个字段，判断tag是不是等于key
+			for i := 0; i < sValue.NumField(); i++ {
+				field := sType.Field(i) // tag信息是存储在类型信息中
+				fileType = field
+				if field.Tag.Get("ini") == key {
+					// 找到对应的字段
+					fieldName = field.Name
+					break
+				}
+			}
+			// 4 如果key = tag, 给这个字段赋值
+			// 4.1 如果key = tag，给这个字段赋值
+			if len(fieldName) == 0 {
+				// 在结构体中找不到对应的字段
+				continue
+			}
+			fileObj := sValue.FieldByName(fieldName)
+			// 4.2 对其赋值
+			fmt.Println(fieldName, fileType.Type.Kind())
+			switch fileType.Type.Kind() {
+			case reflect.String:
+				fileObj.SetString(value)
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				var valueInt int64
+				valueInt, err = strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					err = fmt.Errorf("line:%d value type error", idx+1)
+					return
+				}
+				fileObj.SetInt(valueInt)
+			case reflect.Bool:
+				var valueBool bool
+				valueBool, err = strconv.ParseBool(value)
+				if err != nil {
+					err = fmt.Errorf("line:%d value type error", idx+1)
+					return
+				}
+				fileObj.SetBool(valueBool)
+			case reflect.Float32, reflect.Float64:
+				var valueFloat float64
+				valueFloat, err = strconv.ParseFloat(value, 64)
+				if err != nil {
+					err = fmt.Errorf("line:%d value type error", idx+1)
+					return
+				}
+				fileObj.SetFloat(valueFloat)
+			}
 		}
 	}
 	return

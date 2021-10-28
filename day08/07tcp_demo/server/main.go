156764ea24a502ce3ffcf4f3fd8c9b346f5eb815
sealandsigh <zhujiajunzhu@qq.com>
2021-10-28 15:46:01 +0800

    day08 socket通信初步完成

---
+++ day08/07tcp_demo/server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+// tcp server端
+
+func processConn(conn net.Conn) {
+	// 3 与客户端通信
+	var tmp [128]byte
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		n, err := conn.Read(tmp[:])
+		if err != nil {
+			fmt.Println("read from conn failed,err:", err)
+			return
+		}
+		fmt.Println(string(tmp[:n]))
+		fmt.Print("请说话: ")
+		msg, _ := reader.ReadString('\n') //读到换行
+		msg = strings.TrimSpace(msg)
+		if msg == "exit" {
+			break
+		}
+		conn.Write([]byte(msg))
+	}
+}
+
+func main() {
+	// 1 本地启动端口服务
+	listener, err := net.Listen("tcp", "127.0.0.1:20000")
+	if err != nil {
+		fmt.Println("start tcp server on 127.0.0.1:20000 failed,err:", err)
+		return
+	}
+	for {
+		// 2 等待别人来跟我建立连接
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("accept failed,err:", err)
+			return
+		}
+		go processConn(conn)
+	}
+}

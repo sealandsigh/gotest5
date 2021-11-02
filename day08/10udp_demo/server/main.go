372ffb45a93b4a1fab6b81193e4995a1ca648b67
sealandsigh <zhujiajunzhu@qq.com>
2021-11-02 11:56:11 +0800

    day08 udp传输

---
+++ day08/10udp_demo/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+// UDP server
+
+func main() {
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IPv4(127, 0, 0, 1),
+		Port: 40000,
+	})
+	if err != nil {
+		fmt.Println("Listen UDP failed,err:", err)
+	}
+	defer conn.Close()
+	// 不需要建立连接，直接收发数据
+	var data [1024]byte
+	for {
+		n, addr, err := conn.ReadFromUDP(data[:])
+		if err != nil {
+			fmt.Println("read from UDP failed,err:", err)
+			return
+		}
+		fmt.Println(data[:n])
+		reply := strings.ToUpper(string(data[:n]))
+		// 发送数据
+		conn.WriteToUDP([]byte(reply), addr)
+	}
+}

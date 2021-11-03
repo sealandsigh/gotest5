737517d223fba2c63bb9de17e087213ef3aaaad4
sealandsigh <zhujiajunzhu@qq.com>
2021-11-03 15:48:08 +0800

    day09 http_server完成

--- day09/http_server/main.go
+++ day09/http_server/main.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"io/ioutil"
+	"net/http"
+)
+
 // net/http server
 
-func main() {
+func f1(w http.ResponseWriter, r *http.Request) {
+	str, err := ioutil.ReadFile("./xx.txt")
+	if err != nil {
+		w.Write([]byte("暂无内容"))
+	}
+	w.Write([]byte(str))
+}
 
+func main() {
+	http.HandleFunc("/test/", f1)
+	http.ListenAndServe("127.0.0.1:9090", nil)
 }

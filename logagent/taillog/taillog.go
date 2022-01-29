9a1857849b5bb10c4ada853e338334214d9d8425
sealandsigh <zhujiajunzhu@qq.com>
2022-01-29 16:28:31 +0800

    day12 etcd 删除配置实时调整完毕

--- logagent/taillog/taillog.go
+++ logagent/taillog/taillog.go
@@ -3,6 +3,7 @@ package taillog
 // 专门从日志文件收集日志的模块
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/hpcloud/tail"
@@ -17,12 +18,18 @@ type TailTask struct {
 	path     string
 	topic    string
 	instance *tail.Tail
+	// 为了能实现退出t.run()
+	ctx        context.Context
+	cancelFunc context.CancelFunc
 }
 
 func NewTailTask(path, topic string) (tailObj *TailTask) {
+	ctx, cancel := context.WithCancel(context.Background())
 	tailObj = &TailTask{
-		path:  path,
-		topic: topic,
+		path:       path,
+		topic:      topic,
+		ctx:        ctx,
+		cancelFunc: cancel,
 	}
 	tailObj.init() // 根据路径打开对应的日志
 	return
@@ -41,6 +48,7 @@ func (t *TailTask) init() {
 	if err != nil {
 		fmt.Println("tail file failed, err:", err)
 	}
+	// 当goroutine执行的函数退出的时候，goroutine就退出了
 	go t.run() // 直接去采集日志发送到kafka
 }
 
@@ -63,10 +71,14 @@ func (t *TailTask) init() {
 func (t *TailTask) run() {
 	for {
 		select {
+		case <-t.ctx.Done():
+			fmt.Printf("tail task:%s_%s 结束了...\n", t.path, t.topic)
+			return
 		case line := <-t.instance.Lines: // 从tailObj的通道中一行一行的读取日志数据
 			// 3.2 发往kafka
 			// kafka.SendToKafka(t.topic, line.Text) // 函数调函数
 			// 先把日志数据发到一个通道中
+			fmt.Printf("get log data from %s success, log:%v\n", t.path, line.Text)
 			kafka.SendToChan(t.topic, line.Text)
 			// kafka那个包中有单独的goroutine去取数据发到kafka
 		}

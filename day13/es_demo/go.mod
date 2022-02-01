7b19fb702a6482cbc59189a907f3e8f5587baf87
sealandsigh <zhujiajunzhu@qq.com>
2022-02-01 18:46:33 +0800

    day13 es client

---
+++ day13/es_demo/go.mod
@@ -0,0 +1,10 @@
+module github.com/sealandsigh/gotest5/day13/es_demo
+
+go 1.17
+
+require (
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/olivere/elastic/v7 v7.0.31 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ day13/es_demo/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/olivere/elastic/v7"
+)
+
+// ES insert data demo
+
+// Elasticsearch demo
+
+type Person struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Married bool   `json:"married"`
+}
+
+func main() {
+	client, err := elastic.NewClient(elastic.SetURL("http://xxx:9200"), elastic.SetBasicAuth("elastic", "xxx"))
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	fmt.Println("connect to es success")
+	p1 := Person{Name: "rion", Age: 22, Married: false}
+	// 链式操作
+	put1, err := client.Index().
+		Index("user").
+		BodyJson(p1).
+		Do(context.Background())
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	fmt.Printf("Indexed user %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
+}

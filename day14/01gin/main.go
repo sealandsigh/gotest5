048fa2b5c1075868600409aa112b03bf783d9f28
sealandsigh <zhujiajunzhu@qq.com>
2022-02-11 16:12:57 +0800

    day14 gin demo hello world

--- day14/01gin/main.go
+++ day14/01gin/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 // gin的helloWorld
 
@@ -8,5 +11,9 @@ func main() {
 	// 1. 创建路由
 	r := gin.Default()
 	// 2. 绑定路由规则,执行函数
-	r.GET(aaaaaaaa)
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world!")
+	})
+	// 3. 监听端口，默认在8080
+	r.Run(":8100")
 }

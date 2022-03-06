f9f65bee34898b903e1be30ca5bef1864e23eda9
sealandsigh <zhujiajunzhu@qq.com>
2022-03-06 21:33:36 +0800

    day14 gin单个文件提交

--- day14/01gin/main.go
+++ day14/01gin/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"log"
 )
 
 // gin的helloWorld
@@ -54,19 +54,38 @@ import (
 //	}
 //}
 
+//func main() {
+//	r := gin.Default()
+//	// url参数
+//	r.POST("/form", func(c *gin.Context) {
+//		// 表单参数设置默认值
+//		type1 := c.DefaultPostForm("type", "alert")
+//		// 接收其它的
+//		username := c.PostForm("username")
+//		password := c.PostForm("password")
+//		// 多选框
+//		hobbys := c.PostFormArray("hobby")
+//		c.String(http.StatusOK, fmt.Sprintf("type is %s,username is %s,password is %s,hobbys is %v",
+//			type1, username, password, hobbys))
+//	})
+//	err := r.Run(":8100")
+//	if err != nil {
+//		return
+//	}
+//}
+
+// upload
 func main() {
 	r := gin.Default()
 	// url参数
-	r.POST("/form", func(c *gin.Context) {
-		// 表单参数设置默认值
-		type1 := c.DefaultPostForm("type", "alert")
-		// 接收其它的
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-		// 多选框
-		hobbys := c.PostFormArray("hobby")
-		c.String(http.StatusOK, fmt.Sprintf("type is %s,username is %s,password is %s,hobbys is %v",
-			type1, username, password, hobbys))
+	r.POST("/upload", func(c *gin.Context) {
+		// 表单取文件
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+		// 传到项目根目录,名字就用本身的
+		c.SaveUploadedFile(file, file.Filename)
+		// 打印信息
+		c.String(200, fmt.Sprintf("'%s' upload!", file.Filename))
 	})
 	err := r.Run(":8100")
 	if err != nil {

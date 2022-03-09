50682b18f872ff3ff28b491ab6dc60c73e3c6c71
sealandsigh <zhujiajunzhu@qq.com>
2022-03-09 17:24:47 +0800

    day14 gin html模板渲染

--- day14/01gin/main.go
+++ day14/01gin/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/testdata/protoexample"
 )
 
 type Login struct {
@@ -254,47 +253,69 @@ type Login struct {
 //	}
 //}
 
-// 数据响应,数据渲染
+//// 数据响应,数据渲染
+//func main() {
+//	// 1. 创建路由
+//	// 默认使用了2个中间件Logger(), Recover()
+//	r := gin.Default()
+//	// 1. json
+//	r.GET("/someJSON", func(c *gin.Context) {
+//		c.JSON(200, gin.H{"message": "someJSON", "status": "200"})
+//	})
+//	// 2. 结构体响应
+//	r.GET("/someStruct", func(c *gin.Context) {
+//		var msg struct {
+//			Name    string
+//			Message string
+//			Number  int
+//		}
+//		msg.Name = "root"
+//		msg.Message = "message"
+//		msg.Number = 123
+//		c.JSON(200, msg)
+//	})
+//	// 3. XML
+//	r.GET("/someXML", func(c *gin.Context) {
+//		c.XML(200, gin.H{"message": "abc"})
+//	})
+//	// 4. YAML响应
+//	r.GET("/someYAML", func(c *gin.Context) {
+//		c.YAML(200, gin.H{"name": "zhangsan"})
+//	})
+//	// 5. protobuf格式 谷歌开发的高效存储读取的工具
+//	// 数组？切片？如果自己构建一个传输格式，应该是什么格式?
+//	r.GET("/someProtoBuf", func(c *gin.Context) {
+//		reps := []int64{int64(1), int64(2)}
+//		// 定义数据
+//		label := "label"
+//		// 传protobuf格式数据
+//		data := &protoexample.Test{
+//			Label: &label,
+//			Reps:  reps,
+//		}
+//		c.ProtoBuf(200, data)
+//	})
+//	err := r.Run(":8100")
+//	if err != nil {
+//		return
+//	}
+//}
+
+// html模板渲染
+// gin 支持加载html模板，然后根据模板参数进行配置并返回相应的数据，本质上就是字符串的转换
+// loadHTMLGlob()方法可以加载模板文件
+
 func main() {
 	// 1. 创建路由
 	// 默认使用了2个中间件Logger(), Recover()
 	r := gin.Default()
-	// 1. json
-	r.GET("/someJSON", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "someJSON", "status": "200"})
-	})
-	// 2. 结构体响应
-	r.GET("/someStruct", func(c *gin.Context) {
-		var msg struct {
-			Name    string
-			Message string
-			Number  int
-		}
-		msg.Name = "root"
-		msg.Message = "message"
-		msg.Number = 123
-		c.JSON(200, msg)
-	})
-	// 3. XML
-	r.GET("/someXML", func(c *gin.Context) {
-		c.XML(200, gin.H{"message": "abc"})
-	})
-	// 4. YAML响应
-	r.GET("/someYAML", func(c *gin.Context) {
-		c.YAML(200, gin.H{"name": "zhangsan"})
-	})
-	// 5. protobuf格式 谷歌开发的高效存储读取的工具
-	// 数组？切片？如果自己构建一个传输格式，应该是什么格式?
-	r.GET("/someProtoBuf", func(c *gin.Context) {
-		reps := []int64{int64(1), int64(2)}
-		// 定义数据
-		label := "label"
-		// 传protobuf格式数据
-		data := &protoexample.Test{
-			Label: &label,
-			Reps:  reps,
-		}
-		c.ProtoBuf(200, data)
+	// 加载模板文件
+	r.LoadHTMLGlob("templates/*")
+	//r.LoadHTMLFiles("templates/index.tmpl")
+	r.GET("index", func(c *gin.Context) {
+		// 根据文件名渲染
+		// 最终json将title替换
+		c.HTML(200, "index.tmpl", gin.H{"title": "我的标题"})
 	})
 	err := r.Run(":8100")
 	if err != nil {

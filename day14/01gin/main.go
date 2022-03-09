9e43a320c45aacc4c700e4c9a249ec6056c817c7
sealandsigh <zhujiajunzhu@qq.com>
2022-03-09 16:43:46 +0800

    day14 gin 数据响应数据渲染

--- day14/01gin/main.go
+++ day14/01gin/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/gin-gonic/gin/testdata/protoexample"
 )
 
 type Login struct {
@@ -224,29 +224,77 @@ type Login struct {
 //	}
 //}
 
-// 数据绑定,以下是uri数据绑定方式
+//// 数据绑定,以下是uri数据绑定方式
+//func main() {
+//	// 1. 创建路由
+//	// 默认使用了2个中间件Logger(), Recover()
+//	r := gin.Default()
+//	// json绑定
+//	r.GET("/:user/:password", func(c *gin.Context) {
+//		// 声明接收的变量
+//		var login Login
+//		// Bind()默认解析并绑定form格式
+//		// 根据请求头中的content-type自动推断
+//		if err := c.ShouldBindUri(&login); err != nil {
+//			// 返回错误信息
+//			// gin.H 封装了生成json数据的工具
+//			c.JSON(http.StatusBadGateway, gin.H{"error": err.Error()})
+//			return
+//		}
+//		// 判断用户名密码是否正确
+//		if login.User != "root" || login.Password != "admin" {
+//			c.JSON(http.StatusBadGateway, gin.H{"status": "304"})
+//			return
+//		}
+//		c.JSON(http.StatusOK, gin.H{"status": "200"})
+//	})
+//	err := r.Run(":8100")
+//	if err != nil {
+//		return
+//	}
+//}
+
+// 数据响应,数据渲染
 func main() {
 	// 1. 创建路由
 	// 默认使用了2个中间件Logger(), Recover()
 	r := gin.Default()
-	// json绑定
-	r.GET("/:user/:password", func(c *gin.Context) {
-		// 声明接收的变量
-		var login Login
-		// Bind()默认解析并绑定form格式
-		// 根据请求头中的content-type自动推断
-		if err := c.ShouldBindUri(&login); err != nil {
-			// 返回错误信息
-			// gin.H 封装了生成json数据的工具
-			c.JSON(http.StatusBadGateway, gin.H{"error": err.Error()})
-			return
+	// 1. json
+	r.GET("/someJSON", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "someJSON", "status": "200"})
+	})
+	// 2. 结构体响应
+	r.GET("/someStruct", func(c *gin.Context) {
+		var msg struct {
+			Name    string
+			Message string
+			Number  int
 		}
-		// 判断用户名密码是否正确
-		if login.User != "root" || login.Password != "admin" {
-			c.JSON(http.StatusBadGateway, gin.H{"status": "304"})
-			return
+		msg.Name = "root"
+		msg.Message = "message"
+		msg.Number = 123
+		c.JSON(200, msg)
+	})
+	// 3. XML
+	r.GET("/someXML", func(c *gin.Context) {
+		c.XML(200, gin.H{"message": "abc"})
+	})
+	// 4. YAML响应
+	r.GET("/someYAML", func(c *gin.Context) {
+		c.YAML(200, gin.H{"name": "zhangsan"})
+	})
+	// 5. protobuf格式 谷歌开发的高效存储读取的工具
+	// 数组？切片？如果自己构建一个传输格式，应该是什么格式?
+	r.GET("/someProtoBuf", func(c *gin.Context) {
+		reps := []int64{int64(1), int64(2)}
+		// 定义数据
+		label := "label"
+		// 传protobuf格式数据
+		data := &protoexample.Test{
+			Label: &label,
+			Reps:  reps,
 		}
-		c.JSON(http.StatusOK, gin.H{"status": "200"})
+		c.ProtoBuf(200, data)
 	})
 	err := r.Run(":8100")
 	if err != nil {

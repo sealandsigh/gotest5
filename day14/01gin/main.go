169c6f43137a1d52bcac5958c1a53c7ecc9b1089
sealandsigh <zhujiajunzhu@qq.com>
2022-03-11 17:51:29 +0800

    day14 gin cookie配合中间件的练习

--- day14/01gin/main.go
+++ day14/01gin/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type Login struct {
@@ -457,34 +457,71 @@ type Login struct {
 //	time.Sleep(5 * time.Second)
 //}
 
-// cookie是什么
-// http是无状态协议，服务器不能记录浏览器的访问状态，也就是说服务器不能区分两次请求是否是由同一个客户端发出
-// cookie 就是解决http协议状态的方案之一，中文是小甜饼的意思
-// cookie 实际上就是服务器存在浏览器上的一段信息，浏览器有了cookie之后，每次向服务器发送请求时将该信息发送服务器，服务器收到请求之后，就可以根据该信息处理请求
-// cookie 由服务器创建，并发送给浏览器，最终由浏览器保存
+// --------------------
+
+//// cookie是什么
+//// http是无状态协议，服务器不能记录浏览器的访问状态，也就是说服务器不能区分两次请求是否是由同一个客户端发出
+//// cookie 就是解决http协议状态的方案之一，中文是小甜饼的意思
+//// cookie 实际上就是服务器存在浏览器上的一段信息，浏览器有了cookie之后，每次向服务器发送请求时将该信息发送服务器，服务器收到请求之后，就可以根据该信息处理请求
+//// cookie 由服务器创建，并发送给浏览器，最终由浏览器保存
+//
+//func main() {
+//	// 1. 创建路由
+//	// 默认使用了2个中间件Logger(), Recover()
+//	r := gin.Default()
+//	// 服务端要给客户端cookie
+//	r.GET("/cookie", func(c *gin.Context) {
+//		// 获取客户端是否携带cookie
+//		cookie, err := c.Cookie("key_cookie")
+//		if err != nil {
+//			cookie = "NotSet"
+//			// 给客户端设置cookie
+//			// maxAge int 单位为秒
+//			// path cookie所在目录
+//			// domain string 域名
+//			// secure 是否能通过https访问
+//			// httpOnly bool 是否允许别人通过js获取cookie
+//			c.SetCookie("key_cookie", "value_cookie", 60,
+//				"/", "localhost", false, true)
+//		}
+//		fmt.Printf("cookie的值是: %s\n", cookie)
+//	})
+//
+//	err := r.Run(":8100")
+//	if err != nil {
+//		return
+//	}
+//}
+
+// ------------------------
+// cookie配合中间件的练习
+
+func AuthMiddleWare() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if cookie, err := c.Cookie("abc"); err == nil {
+			if cookie == "123" {
+				c.Next()
+				return
+			}
+		}
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "err"})
+		c.Abort()
+	}
+}
 
 func main() {
-	// 1. 创建路由
-	// 默认使用了2个中间件Logger(), Recover()
+	// 1 创建路由
 	r := gin.Default()
-	// 服务端要给客户端cookie
-	r.GET("/cookie", func(c *gin.Context) {
-		// 获取客户端是否携带cookie
-		cookie, err := c.Cookie("key_cookie")
-		if err != nil {
-			cookie = "NotSet"
-			// 给客户端设置cookie
-			// maxAge int 单位为秒
-			// path cookie所在目录
-			// domain string 域名
-			// secure 是否能通过https访问
-			// httpOnly bool 是否允许别人通过js获取cookie
-			c.SetCookie("key_cookie", "value_cookie", 60,
-				"/", "localhost", false, true)
-		}
-		fmt.Printf("cookie的值是: %s\n", cookie)
+	r.GET("login", func(c *gin.Context) {
+		// 设置cookie
+		c.SetCookie("abc", "123", 60, "/",
+			"localhost", false, true)
+		// 返回信息
+		c.String(200, "Login success!")
 	})
-
+	r.GET("/home", AuthMiddleWare(), func(c *gin.Context) {
+		c.JSON(200, gin.H{"data": "home"})
+	})
 	err := r.Run(":8100")
 	if err != nil {
 		return

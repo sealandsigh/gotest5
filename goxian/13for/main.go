993ab2b23452e3d7ac945c740b43cd504b6616be
sealandsigh <zhujiajunzhu@qq.com>
2022-07-20 19:52:53 +0800

    测试导包

---
+++ goxian/13for/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("循环了一次，当前i是%v", i)
+	}
+}
---
+++ goxian/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sealandsigh/gotest5/goxian/util"
+)
+
+func main() {
+	fmt.Printf("这个是utilnum %v", util.Num1)
+}
